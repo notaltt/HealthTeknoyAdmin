b50f122424ccef5678c507925e95de65e4647408
notaltt <cabanerojeb@gmail.com>
2022-10-13 19:09:17 +0800

    delete all and barcode scan commit

---
+++ app/src/main/java/com/example/healthteknoyadmin/BarcodeScanner.java
@@ -0,0 +1,10 @@
+package com.example.healthteknoyadmin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class BarcodeScanner extends CaptureActivity {
+}
---
+++ app/src/main/java/com/example/healthteknoyadmin/MainActivity.java
@@ -0,0 +1,94 @@
+package com.example.healthteknoyadmin;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseDatabase rootNode;
+    private DatabaseReference reference;
+    private Button deleteButton, scanButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        deleteButton = findViewById(R.id.deleteButton);
+        scanButton = findViewById(R.id.scanButton);
+
+        rootNode = FirebaseDatabase.getInstance("https://teknoyhealthapp-default-rtdb.asia-southeast1.firebasedatabase.app/");
+        reference = rootNode.getReference().child("User");
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                deleteAllBarcode();
+            }
+        });
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                scanBarcode();
+            }
+        });
+
+    }
+
+    public void deleteAllBarcode(){
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    String allUserName = String.valueOf(dataSnapshot.child("username").getValue());
+                    reference.child(allUserName).child("recentExposure").setValue("");
+                    reference.child(allUserName).child("symptoms").setValue("");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    public void scanBarcode(){
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(BarcodeScanner.class);
+        barcodeLauncher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barcodeLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if(result.getContents() != null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("test");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+    });
+}
